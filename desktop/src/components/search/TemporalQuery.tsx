69045717d67b575ae4705eb306c2000da039ce51
ChrisRPL <shepard128@gmail.com>
2026-02-18 21:22:23 +0100

    feat(desktop): update src/components/search/TemporalQuery.tsx

--- desktop/src/components/search/TemporalQuery.tsx
+++ desktop/src/components/search/TemporalQuery.tsx
@@ -202,7 +202,6 @@ export function TemporalQuery({
 }: TemporalQueryProps) {
   const {
     temporalRange,
-    parsedTemporalExpression,
     setTemporalRange,
     clearTemporalRange,
   } = useSearchStore();
